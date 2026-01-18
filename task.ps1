254f3bbddb78fbfd436a8d991b5624d2c7f54fa9
Vladyslav Kozlov <kozlov.vlad@lll.kpi.ua>
2026-01-18 12:29:45 +0200

    The script task.ps1" was implemented

--- task.ps1
+++ task.ps1
@@ -1,1 +1,12 @@
-# Write your code here
+$targetSizeName = "Standard_B2pts_v2"
+$files = Get-ChildItem -Path ".\data" -Filter "*.json"
+
+$results = foreach ($file in $files) {
+    $objectFile = Get-Content $file.FullName | ConvertFrom-Json
+
+    if ($objectFile.Name -eq $targetSizeName) {
+        $file.BaseName
+    }
+}
+
+$results | ConvertTo-Json | Out-File -FilePath ".\result.json" -Encoding utf8
