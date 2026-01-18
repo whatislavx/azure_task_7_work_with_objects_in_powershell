ef51bb2d4229db79042fa4973de84e0fc5090774
Vladyslav Kozlov <kozlov.vlad@lll.kpi.ua>
2026-01-18 12:46:37 +0200

    Fixed the mistake in task.ps1

--- task.ps1
+++ task.ps1
@@ -1,11 +1,12 @@
-$targetSizeName = "Standard_B2pts_v2"
 $files = Get-ChildItem -Path ".\data" -Filter "*.json"
+$targetSizeName = "Standard_B2pts_v2"
+$results = @()
 
-$results = foreach ($file in $files) {
+foreach ($file in $files) {
     $objectFile = Get-Content $file.FullName | ConvertFrom-Json
 
-    if ($objectFile.Name -eq $targetSizeName) {
-        $file.BaseName
+    if ($objectFile | Where-Object { $_.Name -eq $targetSizeName }) {
+        $results += $file.BaseName
     }
 }
 
